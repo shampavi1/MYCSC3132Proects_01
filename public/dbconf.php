926ce432b5021d793a246db2982d337bd74ff524
shampavi1 <139625132+shampavi1@users.noreply.github.com>
2024-12-01 09:17:03 +0000

    try catch added

--- public/dbconf.php
+++ public/dbconf.php
@@ -3,11 +3,11 @@ define('SERVERNAME','127.0.0.1:3306');
 define('USERNAME','root');
 define('PASSWORD','mariadb');
 define('DNAME','school');
-
-//connect with database
-$connect=mysqli_connect(SERVERNAME,USERNAME,PASSWORD,DNAME);
+try{
+    //connect with database
+    $connect=mysqli_connect(SERVERNAME,USERNAME,PASSWORD,DNAME);
     if(!$connect){
-        die("connection failed".mysqli_connect_error());
+       die("connection failed".mysqli_connect_error());
     }
     else{
         echo "connectection successfully";
