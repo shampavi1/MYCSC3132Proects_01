b6aca407659163ed514e26e5f5974817d79db7ee
shampavi1 <139625132+shampavi1@users.noreply.github.com>
2024-12-01 11:47:11 +0000

    index.php updated

---
+++ day02/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Nishanthi Practicals</h1><br><br>
+
+    <a href="table1.php">Table 1</a><br>
+    <a href="table2.php">Table 2</a><br>
+    <a href="table3.php">Table 3</a><br>
+
+    
+</body>
+</html>
