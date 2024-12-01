a3768094e214b2b5450f7e9aa62d914c3a0968d3
shampavi1 <139625132+shampavi1@users.noreply.github.com>
2024-12-01 08:48:31 +0000

    index file fetch data from table

---
+++ public/index.php
@@ -0,0 +1,40 @@
+<?php
+require_once 'conection.php';
+
+try {
+    $sql = "SELECT * FROM STUDENT";
+
+    // Execute the query
+    $result = mysqli_query($connect, $sql);
+
+    // Check if data exists in the table
+    if (mysqli_num_rows($result) > 0) {
+        // Start the table
+        echo "<table border='1' cellpadding='5' cellspacing='0'>";
+        
+        // Fetch the column names to use as table headers
+        echo "<tr>";
+        $columns = mysqli_fetch_fields($result);
+        foreach ($columns as $column) {
+            echo "<th>" . $column->name . "</th>";
+        }
+        echo "</tr>";
+
+        // Fetch the data from rows
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<tr>";
+            foreach ($row as $value) {
+                echo "<td>" . $value . "</td>";
+            }
+            echo "</tr>";
+        }
+
+        // End the table
+        echo "</table>";
+    } else {
+        echo "No result";
+    }
+} catch (Exception $e) {
+    die($e->getMessage());
+}
+?>
