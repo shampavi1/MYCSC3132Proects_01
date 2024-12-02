7e7a3acd800b487dd3fcb90c65b6728996454530
shampavi1 <139625132+shampavi1@users.noreply.github.com>
2024-12-02 17:35:09 +0000

    order changed

--- day03/index.php
+++ day03/index.php
@@ -8,8 +8,8 @@
 <body>
     <h1>My Works 29.11.2024</h1><br><br>
 
-    <a href="form.php">form</a><br>
     <a href="insert.php">Insert</a><br>
+    <a href="form.php">form</a><br>
     <a href="table.php">Table</a><br>
 
     
