19663bef0f83b20f439e5a3a07316b910a0abae0
shampavi1 <139625132+shampavi1@users.noreply.github.com>
2024-12-02 17:40:34 +0000

    some break added

--- day03/index.php
+++ day03/index.php
@@ -9,11 +9,13 @@
     <h1>My Works 29.11.2024</h1><br><br>
 
     echo "you can insert a data enty here";
+    echo "<br>";
     echo "It will carry to form";
+    echo "<br>";
     <a href="insert.php">Insert</a><br>
-    evho "here you can fill the form";
+    evho "here you can fill the form <br>";
     <a href="form.php">form</a><br>
-    echo "here defined data enry added"
+    echo "here defined data enry added <br>"
     <a href="table.php">Table</a><br>
 
     
