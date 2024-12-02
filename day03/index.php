fd012f56a072c1d31e2af2bbede6f33983ca17f4
shampavi1 <139625132+shampavi1@users.noreply.github.com>
2024-12-02 17:45:52 +0000

    some altations

--- day03/index.php
+++ day03/index.php
@@ -8,12 +8,12 @@
 <body>
     <h1>My Works 29.11.2024</h1><br><br>
 
-    echo " you can insert a data enty here <br> ";
-    echo "It will carry to form <br> ";
-    <a href="insert.php">Insert</a><br>
-    echo " Here you can fill the form <br>";
+    <p>"you can insert a data enty here "</p><br>;
+    <p>"It will carry to form "</p><br>;
+    <a href="insert.php">Insert</p><br>
+    <p>"Here you can fill the form"</p><br>;
     <a href="form.php">form</a><br>
-    echo " Here defined data enry added <br>"
+    <p>"Here defined data enry added"</p><br>;
     <a href="table.php">Table</a><br>
 
     
