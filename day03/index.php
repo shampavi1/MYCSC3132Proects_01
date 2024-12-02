223d223453cd0c52ba95076e27d6bb61869cf895
shampavi1 <139625132+shampavi1@users.noreply.github.com>
2024-12-02 17:38:50 +0000

    more clearification added

--- day03/index.php
+++ day03/index.php
@@ -8,8 +8,12 @@
 <body>
     <h1>My Works 29.11.2024</h1><br><br>
 
+    echo "you can insert a data enty here";
+    echo "It will carry to form";
     <a href="insert.php">Insert</a><br>
+    evho "here you can fill the form";
     <a href="form.php">form</a><br>
+    echo "here defined data enry added"
     <a href="table.php">Table</a><br>
 
     
