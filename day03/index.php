3bb4bb08e9a806636678fdd3397e8f28a3114e50
shampavi1 <139625132+shampavi1@users.noreply.github.com>
2024-12-02 17:42:37 +0000

    space for alignment

--- day03/index.php
+++ day03/index.php
@@ -8,14 +8,12 @@
 <body>
     <h1>My Works 29.11.2024</h1><br><br>
 
-    echo "you can insert a data enty here";
-    echo "<br>";
-    echo "It will carry to form";
-    echo "<br>";
+    echo " you can insert a data enty here <br> ";
+    echo "It will carry to form <br> ";
     <a href="insert.php">Insert</a><br>
-    evho "here you can fill the form <br>";
+    echo " Here you can fill the form <br>";
     <a href="form.php">form</a><br>
-    echo "here defined data enry added <br>"
+    echo " Here defined data enry added <br>"
     <a href="table.php">Table</a><br>
 
     
