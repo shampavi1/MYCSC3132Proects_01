fd9ca12c10212b5ad571f0b0c3f4d0b25af91760
shampavi1 <139625132+shampavi1@users.noreply.github.com>
2024-12-02 16:22:05 +0000

    name changed

--- day03/index.php
+++ day03/index.php
@@ -6,7 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Nishanthi Practicals</h1><br><br>
+    <h1>My Works 29.11.2024</h1><br><br>
 
     <a href="form.php">form</a><br>
     <a href="insert.php">Insert</a><br>
