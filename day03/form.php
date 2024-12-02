58ea1ed2a49dd04c0f094387329d22cc90b9e48b
shampavi1 <139625132+shampavi1@users.noreply.github.com>
2024-12-02 16:13:17 +0000

    form.php updated

---
+++ day03/form.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	// file name 
+<form action = "insert.php" method = "POST">
+	<table >
+		<tr >
+			<td align=right>Registration Number: </td>
+			<td><input type="text" name="id"/></td>
+		</tr>
+		<tr>
+			<td align=right>NAME: </td>
+			<td><input type="text" name="name"/></td>
+		</tr>
+			<tr>
+				<td align=right>AGE</td>
+			<td><input type="number" name="age"/></td>
+			</tr>
+			<tr>
+				<td align=right>course</td>
+				<td>
+				<select name= "course">
+				<option value="CS">CS</option>
+				<option value="AMC">AMC</option>
+				<option value="ICT">ICT</option>
+			</select>
+			</td>
+			</tr>
+
+		<tr>
+			<td></td>
+			<td><input type="submit" value="Add a new Student"></td>
+		</tr>
+
+	</table>
+
+</form>
+</body>
+</html>
