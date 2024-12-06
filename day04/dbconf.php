07163cfb91cdd82c5c6496f8ceb835da9f863b41
shampavi1 <139625132+shampavi1@users.noreply.github.com>
2024-12-06 18:17:32 +0000

    dbconf.php updated

---
+++ day04/dbconf.php
@@ -0,0 +1,19 @@
+<?php
+//constant variable (variable name, value)
+define('SERVERNAME', '127.0.0.1:3306');
+define('USERNAME', 'root');
+define('PASSWORD', 'mariadb');
+define('DBNAME', 'school');
+try{
+	
+$connect = mysqli_connect(SERVERNAME,USERNAME,PASSWORD,DBNAME);
+
+if (!$connect) {
+	die("connection failed".mysqli_connect_error()); //die - stop process after that
+}
+}
+catch (Exception $e){
+	die($e->getMessage());
+}
+
+?>
