d157b6805ee26fee78f356f345ca22bfcfd7cc47
shampavi1 <139625132+shampavi1@users.noreply.github.com>
2024-12-06 18:13:58 +0000

    index.php updated

---
+++ day04/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>my Work 06.12.2024</h1><br><br>
+
+    <a href="gettable.php">Gettable</a><br>
+    
+    <a href="myfunc.php">MyFunction</a><br>
+
+
+    
+</body>
+</html>
