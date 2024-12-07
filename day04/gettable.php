4a9ce0058a3e65883d955d84695ffe5c6288819f
shampavi1 <139625132+shampavi1@users.noreply.github.com>
2024-12-07 14:45:34 +0000

    gettable.php updated

---
+++ day04/gettable.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>EMPLOYEE LIST</title>
+</head>
+<body>
+
+	<?php
+
+//db connection file
+require_once 'dbconf.php'; //(conf/dbconf.php) (folder/file)
+require_once 'myfunc.php';
+
+//PrintTable ("employee",$connect);
+user($connect);
+
+?>
+
+
+
+</body>
+</html>
