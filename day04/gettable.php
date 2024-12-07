c2367e0f2b5a67b64e11c8c7035746caa253862e
shampavi1 <139625132+shampavi1@users.noreply.github.com>
2024-12-07 14:59:53 +0000

    syntex

--- day04/gettable.php
+++ day04/gettable.php
@@ -11,11 +11,8 @@
 require_once 'dbconf.php'; //(conf/dbconf.php) (folder/file)
 require_once 'myfunc.php';
 
-//PrintTable ("employee",$connect);
+//PrintTable
 user($connect);
-$user_id = $_GET['user_id'];
-
-Empdetails($user_id,$connect);
 
 ?>
 
