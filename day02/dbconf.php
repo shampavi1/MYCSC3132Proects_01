fef937d9509eb775e5b8ae861b35d08d89e9386a
shampavi1 <139625132+shampavi1@users.noreply.github.com>
2024-12-07 14:11:12 +0000

    db name changed

--- day02/dbconf.php
+++ day02/dbconf.php
@@ -3,7 +3,7 @@
 define('SERVERNAME', '127.0.0.1:3306');
 define('USERNAME', 'root');
 define('PASSWORD', 'mariadb');
-define('DBNAME', 'school');
+define('DBNAME', 'students');
 try{
 	//connect with database
 $connect = mysqli_connect(SERVERNAME,USERNAME,PASSWORD,DBNAME);
