da3a5c3d7b8230f4caf3e896cd122d8efbd1b065
shampavi1 <139625132+shampavi1@users.noreply.github.com>
2024-12-07 14:50:29 +0000

    tabe name updated

--- day04/myfunc.php
+++ day04/myfunc.php
@@ -2,98 +2,99 @@
 // database connection file
 require_once 'dbconf.php';
 
- function user($connect){ 
-            try{
-                
-                $sql = "SELECT user_id, user_name FROM  user";
+function user($connect){ 
+    try{
+        
+        $sql = "SELECT user_id, user_name FROM  users";
+    
+    
+        $result = mysqli_query($connect,$sql);
+    
+        if (mysqli_num_rows($result)>0) {
+        
+    
+            echo "<table border=1> ";
+    
+        $col = mysqli_fetch_fields($result);
+    
+        echo "<tr>";
+        foreach ($col as $value) {
             
-            
-                $result = mysqli_query($connect,$sql);
-            
-                if (mysqli_num_rows($result)>0) {
-                
-            
-                    echo "<table border=1> ";
-            
-                $col = mysqli_fetch_fields($result);
-            
-                echo "<tr>";
-                foreach ($col as $value) {
-                    
-                        echo "<td>".$value->name."</td>";
-                    }
-
-                    echo "<td> View details </td>";
-                    echo "</tr>";
-                    while($row = mysqli_fetch_assoc($result)){ 
-                    echo "<tr>";
-                    foreach ($row as $key => $value) {
-                        echo "<td>$value</td>";
-                    }
-                    $user_id=$row['user_id'];
-                    //query string
-                    echo "<td><a href='printtable.php? user_id=$user_id '> View </a> </td>";
-                    echo "</tr>";
-                    
-                    }
-
-                    echo "</table>";
-                } 
-                else{
-                    echo "No results"; 
-                }
-            
-                }
-            
-            
-            catch(Exception $e){
-                die($e->getMessage());
+                echo "<td>".$value->name."</td>";
             }
-        }
 
-
-
-         function Empdetails($user_id,$connect){
-            try{
-            
-                $sql = "SELECT * FROM  user where  user_id = $user_id ";
-            
-            
-                $result = mysqli_query($connect,$sql);
-            
-                if (mysqli_num_rows($result)>0) {
-                
-            
-                    echo "<table border=1> ";
-            
-                $col = mysqli_fetch_fields($result);
-            
-                echo "<tr>";
-                foreach ($col as $value) {
-                    
-                        echo "<td>".$value->name."</td>";
-                    }
-                    echo "</tr>";
-                    while($row = mysqli_fetch_assoc($result)){
-                        
-                    echo "<tr>";
-                    foreach ($row as $key => $value) {
-                        echo "<td>$value</td>";
-                    }
-                    echo "</tr>";
-                    }
-                    echo "</table>";
-                } 
-                else{
-                    echo "No results"; 
-                }
-            
-                }
-            
-            
-            catch(Exception $e){
-                die($e->getMessage());
+            echo "<td> View details </td>";
+            echo "</tr>";
+            while($row = mysqli_fetch_assoc($result)){ 
+            echo "<tr>";
+            foreach ($row as $key => $value) {
+                echo "<td>$value</td>";
             }
+            $user_id=$row['user_id'];
+            //query string
+            echo "<td><a href='printtable.php? user_id=$user_id '> View </a> </td>";
+            echo "</tr>";
+            
+            }
+
+            echo "</table>";
+        } 
+        else{
+            echo "No results"; 
         }
+    
+        }
+    
+    
+    catch(Exception $e){
+        die($e->getMessage());
+    }
+}
+
+
+
+ function Empdetails($user_id,$connect){
+    try{
+    
+        $sql = "SELECT * FROM  users where  user_id = $user_id ";
+    
+    
+        $result = mysqli_query($connect,$sql);
+    
+        if (mysqli_num_rows($result)>0) {
+        
+    
+            echo "<table border=1> ";
+    
+        $col = mysqli_fetch_fields($result);
+    
+        echo "<tr>";
+        foreach ($col as $value) {
+            
+                echo "<td>".$value->name."</td>";
+            }
+            echo "</tr>";
+            while($row = mysqli_fetch_assoc($result)){
+                
+            echo "<tr>";
+            foreach ($row as $key => $value) {
+                echo "<td>$value</td>";
+            }
+            echo "</tr>";
+            }
+            echo "</table>";
+        } 
+        else{
+            echo "No results"; 
+        }
+    
+        }
+    
+    
+    catch(Exception $e){
+        die($e->getMessage());
+    }
+}
 
 ?>
+
