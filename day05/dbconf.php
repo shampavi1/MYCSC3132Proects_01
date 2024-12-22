aab4ba395133d71a1d452843f9c6d390564461c3
shampavi1 <139625132+shampavi1@users.noreply.github.com>
2024-12-22 03:37:20 +0000

    dbconf updated

---
+++ day05/dbconf.php
@@ -0,0 +1,9 @@
+<?php
+	
+	define('SERVERNAME', '127.0.0.1:3306');
+	define('USERNAME', 'root');
+	define('PASSWORD', 'mariadb');
+	define('DBNAME', 'icae02');
+	
+	$connection = mysqli_connect(HOST,USERNAME,PWD,DB);
+?>
